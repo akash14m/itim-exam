968b65ce530fea4e747613bf74591b992b7d3009
akash14m <88096729+akash14m@users.noreply.github.com>
2021-07-29 12:39:39 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main(){
+        int a, b;
+        printf("\nEnter Number1: ");
+        scanf("%d", &a);
+        printf("\nEnter Number2: ");
+        scanf("%d", &b);
+        printf("\nAddition is: %d\n", a+b);
+}
